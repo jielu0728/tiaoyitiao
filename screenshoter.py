9d061d8f1c3aa4bdf4bdfef785237589c74f7239
JieLU <jielu0728@gmail.com>
2018-01-13 00:18:32 +0100

    position of person got

--- screenshoter.py
+++ screenshoter.py
@@ -11,14 +11,20 @@ im = np.array(im)
 
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-edge_detected_image = cv2.Canny(im_gray, 30, 200)
-_, contours, _= cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+circles = cv2.HoughCircles(im_gray,cv2.HOUGH_GRADIENT,1.2,100,
+                            param1=50,param2=30,minRadius=20,maxRadius=30)
+
+head = circles[0][0]
+foot = head
+foot[1] += 114
+cv2.circle(im, (foot[0], foot[1]), 2, (0, 0, 255), 3)
+
+#edge_detected_image = cv2.Canny(im_gray, 30, 200)
+#_, contours, _= cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#cv2.drawContours(im_gray, contours,  -1, (0,0,0), 2)
 
 
-
-cv2.drawContours(im_gray, contours,  -1, (0,0,0), 2)
-
-
-cv2.imshow('compressed', im_gray )
+cv2.imshow('foot', im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
