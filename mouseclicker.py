7c1b1b6025d9563b650f0cd876970b8f136cfc43
JieLU <jielu0728@gmail.com>
2018-01-14 23:50:15 +0100

    first runnable version

--- mouseclicker.py
+++ mouseclicker.py
@@ -1,5 +1,8 @@
 import pyautogui
 import time
+import random
+
+from screenshoter import DistanceDetector
 
 pyautogui.FAIL_SAFE = True
 pyautogui.PAUSE = 1
@@ -9,8 +12,14 @@ y_button = 1200
 
 pyautogui.click(x=x_button, y=y_button)
 
+distance_detector = DistanceDetector()
+
 while True:
-    time.sleep(2)
+    time.sleep(random.uniform(0.5, 2))
+    distance_detector.screen_shoot()
+    distance_detector.foot_detection()
+    distance_detector.find_center()
+    distance = distance_detector.get_distance()
     print('start clicking')
-    pyautogui.dragTo(x=x_button, y=y_button, duration=0.5)
+    pyautogui.dragTo(x=x_button, y=y_button, duration=distance*0.0018)
     print('released')
--- screenshoter.py
+++ screenshoter.py
@@ -2,84 +2,93 @@ import pyscreenshot
 import numpy as np
 import cv2
 
-x1 = 66
-x2 = 410 + x1
-y1 = 300
-y2 = 400 + y1
-im = pyscreenshot.grab(bbox=(x1, y1, x2, y2))
-im = np.array(im)
 
-im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+class DistanceDetector:
+    x1 = 66
+    x2 = 760 + x1
+    y1 = 450
+    y2 = 800 + y1
 
-#foot detection
-circles = cv2.HoughCircles(im_gray,cv2.HOUGH_GRADIENT,1.2,100,
-                            param1=50,param2=30,minRadius=20,maxRadius=30)
+    def screen_shoot(self):
+        im = pyscreenshot.grab(bbox=(self.x1, self.y1, self.x2, self.y2))
+        self.im = np.array(im)
+        self.im_gray = cv2.cvtColor(self.im, cv2.COLOR_BGR2GRAY)
 
-head = circles[0][0]
-foot = head
-foot[1] += 130
-cv2.circle(im, (foot[0], foot[1]), 2, (0, 0, 255), 3)
-#edge_detected_image = cv2.Canny(im_gray, 30, 200)
-#_, contours, _= cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#cv2.drawContours(im_gray, contours,  -1, (0,0,0), 2)
+        # getting background color range
+        left_top = self.im[0][0]
+        self.b_range_background = (left_top[0] - 10, left_top[0] + 10)
+        self.g_range_background = (left_top[1] - 10, left_top[1] + 10)
+        self.r_range_background = (left_top[2] - 40, left_top[2] + 10)
 
-#center detection
-edges = cv2.Canny(im_gray,50,150,apertureSize = 3)
-cv2.imwrite("edge.png",edges)
-print(edges.shape)
-def squre(img, tmp):
-    indexes = []
-    l,r = img.shape
-    for i in range(l):
-        for j in range(r):
-            if img[i][j] == 255 and abs(tmp - j) <= 2 and tmp >= j:
-                indexes.append((i, j))
-                tmp = j
+    def foot_detection(self):
+        circles = cv2.HoughCircles(self.im_gray,cv2.HOUGH_GRADIENT,1.2,100,
+                                    param1=50,param2=20,minRadius=20,maxRadius=25)
+        head = circles[0][0]
+        self.foot = head
+        self.foot[1] += 114
+        #cv2.circle(im, (foot[0], foot[1]), 2, (0, 0, 255), 3)
+
+    def is_background(self, color):
+        return color[0] >= self.b_range_background[0] and color[0] <= self.b_range_background[1] and\
+               color[1] >= self.g_range_background[0] and color[1] <= self.g_range_background[1] and\
+               color[2] >= self.r_range_background[0] and color[2] <= self.r_range_background[1]
+
+
+    def check_color_sim(self, color1, color2):
+        if abs(int(color1[0]) - int(color2[0])) > 30 or abs(int(color1[1]) - int(color2[1])) > 30 or\
+                abs(int(color1[2]) - int(color2[2])) > 30:
+            return False
+        return True
+
+    def find_first_point(self, im):
+        for y in range(im.shape[0]):
+            for x in range(im.shape[1]):
+                color = im[y][x]
+                color_down = im[y+1][x]
+                if not self.is_background(color) and self.check_color_sim(color, color_down):
+                    return (y, x)
+
+    def find_left_right_point(self, im, first_point, foot):
+        y_top, x_top = first_point
+        x_range_left = range(x_top, max(int(foot[0]), x_top - 300), -1) if foot[1] < x_top else \
+            range(x_top, x_top - 300, -1)
+        x_range_right = range(x_top, min(int(foot[0]), x_top + 300)) if foot[1] > x_top else \
+            range(x_top, min(x_top + 300, im.shape[1] - 1))
+        y_range = range(y_top, y_top + 300)
+        sim_colors = []
+        for y in y_range:
+            if self.is_background(im[y][x_top]):
                 break
-
-    x = indexes[0][1]
-    for i,j in indexes:
-        if img[i][j]==img[i+1][j] and img[i][j]==img[i+2][j] and img[i][j]==img[i+3][j]:
-            y = i
-            break
-    print("squre")
-    return (y,x)
-
-
-def oval(img, indexes):
-    ys = [j for i,j in indexes]
-    x = indexes[0][1]
-    for i in range(len(indexes)):
-        if ys[i] == ys[i + 1] and ys[i] == ys[i + 2] and ys[i] == ys[i + 3] and ys[i] == ys[i + 4] and ys[i] == ys[
-                    i + 5]:
-            y = indexes[i][0]
-            break
-
-    print("oval")
-    return (y+5,x+5)
+            for x in x_range_left:
+                if self.is_background(im[y][x]):
+                    break
+                if self.check_color_sim(im[y][x], im[first_point[0]][first_point[1]]):
+                    sim_colors.append((y, x))
+            for x in x_range_right:
+                if self.is_background(im[y][x]):
+                    break
+                if self.check_color_sim(im[y][x], im[first_point[0]][first_point[1]]):
+                    sim_colors.append((y, x))
+        if foot[1] < x_top:
+            return max(sim_colors, key = lambda x: x[1])
+        return min(sim_colors, key=lambda x: x[1])
 
 
-def find_center(img):
-    indexes = []
-    l,r = img.shape
-    for i in range(l):
-        for j in range(r):
-            if img[i][j] == 255:
-                indexes.append((i, j))
-                break
-    _, tmp = indexes[0]
-
-    if indexes[0][1] - indexes[1][1] < 4:
-        y,x = squre(img, tmp)
-    else:
-        y,x = oval(img, indexes)
-    return y,x
+    def find_center(self):
+        first_point = self.find_first_point(self.im)
+        another_point = self.find_left_right_point(self.im, first_point, self.foot)
+        self.center = (first_point[1], another_point[0])
 
 
-y, x = find_center(edges)
+    def get_distance(self):
+        return np.math.hypot(self.foot[0] - self.center[0], self.foot[1] - self.center[1])
 
-cv2.circle(im, (x, y), 2, (0, 0, 255), 3)
+
+'''
+cv2.circle(im, (first_point[1], another_point[0]), 2, (0, 0, 255), 3)
+
+
 cv2.imshow('foot', im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+'''
